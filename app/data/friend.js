0c47a98bc39d5522cff26e0352e0a3b35d207619
Ray Riney <rayfordriney@gmail.com>
2020-01-25 13:28:50 -0800

    first commit

---
+++ app/data/friend.js
@@ -0,0 +1,39 @@
+var friendsArray = [{
+  "name": "Dr Gonzo",
+  "photo": "app/public/img/friend1.jpeg",
+  "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+  "name": "Lucy",
+  "photo": "app/public/img/Lucy.jpeg",
+  "scores": [1, 2, 1, 1, 3, 2, 2, 1, 3, 2]
+},
+{
+  "name": "My Lawyer",
+  "photo": "app/public/img/mylawyer.jpeg",
+  "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+},
+{
+  "name": "Flea",
+  "photo": "app/public/img/whatsthetrouble.jpeg",
+  "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+},
+{
+  "name": "Back Door Beauty?",
+  "photo": "app/public/img/backdoorbeauty.jpeg",
+  "scores": [4, 3, 1, 1, 5, 2, 5, 3, 1, 4]
+},
+{
+  "name": "Sven",
+  "photo": "app/public/img/fuofficerimincharge.jpeg",
+  "scores": [3, 3, 4, 5, 4, 3, 5, 3, 5, 5]
+},
+{
+  "name": "Awful Jackass",
+  "photo": "app/public/img/Awful Jackass.jpeg",
+  "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+}
+
+]
+
+module.exports = friendsArray;
